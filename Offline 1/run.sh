fb79c7b9d81b3c656cf428f9bdbb074cb6c6d2f6
ramisa2108 <1705004@ugrad.cse.buet.ac.bd>
2022-10-12 20:49:45 +0600

    fixed run.sh

--- Offline 1/run.sh
+++ Offline 1/run.sh
@@ -1,5 +1,8 @@
 # run with cpp file name as argument
-
+if [ $# == 0 ]; then
+	echo "run with cpp filename"
+	exit
+fi
 g++ $1 -o main -lglut -lGLU -lGL
 
 if [ $? -eq 0 ]; then
--- Offline 2/run.sh
+++ Offline 2/run.sh
@@ -1,5 +1,8 @@
 # run with cpp file name as argument
-
+if [ $# == 0 ]; then
+	echo "run with cpp filename"
+	exit
+fi
 g++ $1 -o main
 
 if [ $? -eq 0 ]; then
